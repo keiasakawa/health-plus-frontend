a9a650c35392a58fa5f710d15ad3ec1ef63fda85
Kei Asakawa <77090883+Kaiyooon@users.noreply.github.com>
2023-03-08 23:02:12 -0800

    Map the user's allergies to warning

--- health-plus/src/pages/recipe/RecipePage.tsx
+++ health-plus/src/pages/recipe/RecipePage.tsx
@@ -30,6 +30,25 @@ const RecipePage: React.FC = () => {
     const [allergies, setAllergies] = useState(new Set());
     const history = useHistory();
 
+    const map = {'milk': 'Milk', 
+    'eggs': 'Eggs', 
+    'fish': 'Fish (e.g., bass, flounder, cod)', 
+    'bass': 'Fish (e.g., bass, flounder, cod)', 
+    'flounder': 'Fish (e.g., bass, flounder, cod)', 
+    'halibut': 'Fish (e.g., bass, flounder, cod)', 
+    'cod': 'Fish (e.g., bass, flounder, cod)', 
+    'wheat': 'Wheat', 
+    'crab': 'Crustacean shellfish (e.g., crab, lobster, shrimp)',
+    'lobster': 'Crustacean shellfish (e.g., crab, lobster, shrimp)', 
+    'shrimp': 'Crustacean shellfish (e.g., crab, lobster, shrimp)', 
+    'nut': 'Tree nuts (e.g., almonds, walnuts, pecans)', 
+    'almond': 'Tree nuts (e.g., almonds, walnuts, pecans)', 
+    'walnut': 'Tree nuts (e.g., almonds, walnuts, pecans)', 
+    'pecan': 'Tree nuts (e.g., almonds, walnuts, pecans)', 
+    'peanut': 'Peanuts', 
+    'soybean': 'Soybeans'}
+    const commonAllergies2 = new Map(Object.entries(map));
+
     const commonAllergies = ['milk', 'eggs', 'fish', 'bass', 'flounder', 'halibut', 'cod', 'wheat', 'crab', 'lobster', 'shrimp', 'nut', 'almond', 'walnut', 'pecan', 'peanut', 'soybean']
 
 
@@ -40,9 +59,7 @@ const RecipePage: React.FC = () => {
             for (let i = 0; i < commonAllergies.length; i ++) {
                 for ( let j = 0; j < ingredients.length; j ++) {
                     if (ingredients[j].toLowerCase().includes(commonAllergies[i])) {
-                        console.log(ingredients[j].toLowerCase())
-                        console.log(commonAllergies[i])
-                        warnings.add(commonAllergies[i])
+                        warnings.add(commonAllergies2.get(commonAllergies[i]))
                     }
                 }
             }
