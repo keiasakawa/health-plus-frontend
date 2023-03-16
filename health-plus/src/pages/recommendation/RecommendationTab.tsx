c27c375382a41b157d9040c583ca06f9db925b8e
ericluuv <ericluuv@gmail.com>
2023-03-15 18:01:37 -0700

    Updates recommendation page everytime it's viewed now.

--- health-plus/src/pages/recommendation/RecommendationTab.tsx
+++ health-plus/src/pages/recommendation/RecommendationTab.tsx
@@ -1,10 +1,22 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent} from '@ionic/react';
+import { 
+  IonContent, 
+  IonHeader, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar, 
+  IonCard, 
+  IonCardHeader, 
+  IonCardTitle, 
+  IonCardSubtitle, 
+  IonCardContent, 
+  useIonViewWillEnter,
+} from '@ionic/react';
 import ExploreContainer from '../../components/ExploreContainer';
 import './RecommendationTab.css';
 import { dummyMeal, recommendedMeal } from './heuristics';
 import Header from '../../components/Header';
 import { HealthKit, HealthKitOptions } from '@awesome-cordova-plugins/health-kit';
-import { useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import { instance } from '../../utils';
 import { useHistory } from 'react-router-dom';
 import LoginFilter from '../../filter/LoginFilter';
@@ -23,12 +35,12 @@ const options: HealthKitOptions = {
 
 const requestAuthorization = async () => {
   // Need to run this before any healthkit stuff
-  if (! (await HealthKit.available())) {
+  if (!(await HealthKit.available())) {
     console.log("HealthKit is not available on this app.");
     return false;
   }
   for (const type of types) {
-    const authRes = await HealthKit.checkAuthStatus({'type': type});
+    const authRes = await HealthKit.checkAuthStatus({ 'type': type });
     if (authRes !== 'authorized') {
       await HealthKit.requestAuthorization(options);
       // Will request everything
@@ -40,6 +52,7 @@ const requestAuthorization = async () => {
   return true;
 }
 
+
 const getHealthData = async () => {
   // Returns the health data needed from HealthKit to recommend a meal.
   const res = await requestAuthorization();
@@ -49,27 +62,27 @@ const getHealthData = async () => {
     'unit': 'lb'
   }))?.value;
   console.log("Weight: ", weight || "Unknown Weight");
-  const height = await HealthKit.readHeight({"unit": "in"});
+  const height = await HealthKit.readHeight({ "unit": "in" });
   const gender = await HealthKit.readGender();
   console.log("The Gender: ", gender);
   const dob = (await HealthKit.readDateOfBirth()).substring(0, 10);
   console.log("Date of Birth: ", dob);
   const activeCal = await HealthKit.querySampleType({
     'startDate': new Date(new Date().getTime() - (24 * 60 * 60 * 1000)),
-          'endDate': new Date(), // now
-          'sampleType': 'HKQuantityTypeIdentifierActiveEnergyBurned',
-          'unit': 'Cal' 
+    'endDate': new Date(), // now
+    'sampleType': 'HKQuantityTypeIdentifierActiveEnergyBurned',
+    'unit': 'Cal'
   });
   const restCal = await HealthKit.querySampleType({
     'startDate': new Date(new Date().getTime() - (24 * 60 * 60 * 1000)),
-          'endDate': new Date(), // now
-          'sampleType': 'HKQuantityTypeIdentifierBasalEnergyBurned',
-          'unit': 'Cal'
+    'endDate': new Date(), // now
+    'sampleType': 'HKQuantityTypeIdentifierBasalEnergyBurned',
+    'unit': 'Cal'
   });
   const allCalArr = activeCal.concat(restCal);
   let total_cal = 0;
   for (const calorie_obj of allCalArr) {
-    total_cal += calorie_obj?.quantity; 
+    total_cal += calorie_obj?.quantity;
   }
   console.log({
     weight: weight,
@@ -89,34 +102,32 @@ const getHealthData = async () => {
 
 
 const RecommendationTab: React.FC = () => {
-  const [mealData, setData] = useState([{
-    id: '', 
-    meal_name:'', 
-    calories:0, 
-    total_fat:0,
-    carbohydrates:0,
-    protein:0, 
-    image_url:'',
-    meal_description:'',
-  }]);
   const history = useHistory();
-  useEffect(() => {
-    (async () => {
-      const healthkitAuthorized = await requestAuthorization();
-      if (healthkitAuthorized) {
-        const healthData = await getHealthData();
-        const headers = {
-          Authorization: "Bearer " + localStorage.getItem("token")
-        };
-        const response = await instance.post('/meals/recommended', healthData, {headers});
-        console.log(response.data);
-        if (response?.data?.length == 0) { response.data = []; }
-        setData(response.data); // So when post is empty, no cards show up instead of the default one
-      } else {
-        setData([]);
-      }
-    })();
-  }, []);
+  const [mealData, setData] = useState([{
+    id: '',
+    meal_name: '',
+    calories: 0,
+    total_fat: 0,
+    carbohydrates: 0,
+    protein: 0,
+    image_url: '',
+    meal_description: '',
+  }]);
+  useIonViewWillEnter(async () => {
+    const healthkitAuthorized = await requestAuthorization();
+    if (healthkitAuthorized) {
+      const healthData = await getHealthData();
+      const headers = {
+        Authorization: "Bearer " + localStorage.getItem("token")
+      };
+      const response = await instance.post('/meals/recommended', healthData, { headers });
+      console.log(response.data);
+      if (response?.data?.length == 0) { response.data = []; }
+      setData(response.data); // So when post is empty, no cards show up instead of the default one
+    } else {
+      setData([]);
+    }
+  });
 
   const emptyMessage = (mealData: Array<object>) => {
     if (mealData.length == 0) { return "No meals to show!"; }
@@ -126,7 +137,7 @@ const RecommendationTab: React.FC = () => {
   const redirect = (id: string) => {
     history.push({
       pathname: `meal/${id}`,
-      state: {detail: 'recommendations'}
+      state: { detail: 'recommendations' }
     })
   }
 
@@ -144,18 +155,18 @@ const RecommendationTab: React.FC = () => {
         {mealData.map(recipe => {
           return (
             <>
-            <IonCard key={recipe.id} onClick={() => {redirect(recipe.id);}}>
-              <img width="20%" alt= "Recipe" src={recipe.image_url} />
-              <IonCardTitle text-wrap>{recipe.meal_name}</IonCardTitle>
-              <IonCardHeader>
-                <IonCardSubtitle>Calories: {recipe.calories}, Protein: {recipe.protein}, Carbohydrates: {recipe.carbohydrates}, Fats: {recipe.total_fat}</IonCardSubtitle>
-              </IonCardHeader>
-              <IonCardContent>
-                {recipe.meal_description}
-              </IonCardContent>
-            </IonCard> 
+              <IonCard key={recipe.id} onClick={() => { redirect(recipe.id); }}>
+                <img width="20%" alt="Recipe" src={recipe.image_url} />
+                <IonCardTitle text-wrap>{recipe.meal_name}</IonCardTitle>
+                <IonCardHeader>
+                  <IonCardSubtitle>Calories: {recipe.calories}, Protein: {recipe.protein}, Carbohydrates: {recipe.carbohydrates}, Fats: {recipe.total_fat}</IonCardSubtitle>
+                </IonCardHeader>
+                <IonCardContent>
+                  {recipe.meal_description}
+                </IonCardContent>
+              </IonCard>
             </>
-        )
+          )
         })}
       </IonContent>
     </IonPage>
